27c02ed90e0b17c72a52ea92726d5ba776e9d071
Thomas Windt <thomas@architekt-windt.de>
2018-06-24 13:03:18 +0200

    Update
    Signed-off-by: Thomas Windt <thomas@architekt-windt.de>

--- tachometer/sketch.js
+++ tachometer/sketch.js
@@ -2,7 +2,7 @@ var locationData;
 function preload(){
     locationData =  getCurrentPosition();
 }
-
+var p;
 function setup() {
     print(locationData.latitude)
     print(locationData.longitude)
@@ -11,4 +11,11 @@ function setup() {
     print(locationData.altitudeAccuracy)
     print(locationData.heading)
     print(locationData.speed)
+    p =
+}
+
+function draw(){
+  createP(locationData.latitude);
+  createP(locationData.longitude);
+  createP(locationData.speed);
 }
